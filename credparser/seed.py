c3fac607fcbab8be81819264c0c0eb9092a2f732
na-parse <nathan@unit03.net>
2025-06-26 12:46:22 -0700

    expanded inner documentation for seed behavior

--- credparser/seed.py
+++ credparser/seed.py
@@ -2,7 +2,10 @@
 credparser / seed.py
 
 Provider for the master key seed
+'''
+MASTER_SEED = "This is the default seed value, please change me!!!"
 
+'''
 USAGE:
   This script must set a MASTER_SEED value at the end of import/load.
   
@@ -15,6 +18,51 @@ USAGE:
     integrated with a secrets system to provide further security around the
     master seed, and the code necessary can be implemented here, ensuring 
     that the result is stored in MASTER_SEED.
-'''
+  
+  The MASTER_SEED _MUST_ remain consistent across executions, otherwise
+    previously generated credentials will fail to decode.
 
-MASTER_SEED = "This is the default seed value, please change me!!!"
+Other examples of how to generate/maintain a MASTER_SEED:
+
+# Master Seed file 
+
+Use `secrets` or another method to generate an external file to use as a master seed and read here:
+
+```python
+# Create a master_seed file
+import secrets
+with open('.master_seed','wb') as f:
+  f.write(secrets.token_bytes(128))
+```
+
+Modify credparser/seed.py to read this file:
+```python
+import hashlib
+with open('.master_seed','rb') as f:
+  seed_bin = f.read()
+  # Seed must be set as a string (for now, updates roadmapped)
+  MASTER_SEED = hashlib.sha256(seed_bin).hexdigest()
+```
+
+# Hash the seed.py file itself and modify with comment/filler text
+
+Take advantage of the seed.py file itself by modifying this file and hashing it directly.
+
+```python
+import hashlib
+with open(__file__,'rb') as f:
+  h = hashlib.sha256
+  for chunk in iter(lambda: f.read(8192),b''):
+    h.update(chunk)
+MASTER_SEED = h.hexdigest()
+```
+
+Reminder - Ultimately for the package to be usable, the MASTER_SEED will need to be
+  visible or accessible by any user/system account that needs to be able to encode
+  and decode credentials, and thus is it not a secure system.
+
+The credential string's SALT value is the 'private' part of the equation and as such
+  credential strings should still be treated as sensitive information and protected
+  with appropriate filesystem security standards.
+
+'''
